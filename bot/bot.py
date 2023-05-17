e61ab66c4960dd07f653d0c58bd3fdcee60369f7
techus <llc.bissoft@gmail.com>
2023-05-17 10:52:18 +0400

    Change greeting message

--- bot/bot.py
+++ bot/bot.py
@@ -120,7 +120,7 @@ async def start_handle(update: Update, context: CallbackContext):
     db.set_user_attribute(user_id, "last_interaction", datetime.now())
     db.start_new_dialog(user_id)
 
-    reply_text = "Привет! Давай начнем новый диалог! /new. Или можешь подробнее узнать, что я умею /help \n"
+    reply_text = "Привет! Я чат-бот-ассистент, готовый помочь тебе в любых вопросах. С чего начнем?\n"
 
     await update.message.reply_text(reply_text, parse_mode=ParseMode.HTML)
 
